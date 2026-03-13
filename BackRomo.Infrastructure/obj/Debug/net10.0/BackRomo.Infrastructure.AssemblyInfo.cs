db2e9b9f3c7b5b82ed213a3243a0bbfb0878dcd5
Marcos Hernández Barba <marcoshernandez171711@gmail.com>
2026-03-12 19:55:28 -0500

    Actulizacion de compilacion

--- BackRomo.Infrastructure/obj/Debug/net10.0/BackRomo.Infrastructure.AssemblyInfo.cs
+++ BackRomo.Infrastructure/obj/Debug/net10.0/BackRomo.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackRomo.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50fca54c8b2580e569eda456f9050aac3ddfe5b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6fd08dae424d2f1890186195b0464dc99cbb4a7c")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackRomo.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackRomo.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
