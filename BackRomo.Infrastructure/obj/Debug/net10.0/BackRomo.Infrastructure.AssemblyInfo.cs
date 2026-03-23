b93b78d86e35186c343ba83f9a29d8737ede039b
Marcos Hernández Barba <marcoshernandez171711@gmail.com>
2026-03-23 17:51:37 -0500

    Modificacion de compilacion del aplicativo

--- BackRomo.Infrastructure/obj/Debug/net10.0/BackRomo.Infrastructure.AssemblyInfo.cs
+++ BackRomo.Infrastructure/obj/Debug/net10.0/BackRomo.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackRomo.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f7245bc9411e2040d619e8a1cd7f70d0a2307e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68958701a8f0e92dda490f1f2d7172ffd6021c32")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackRomo.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackRomo.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
