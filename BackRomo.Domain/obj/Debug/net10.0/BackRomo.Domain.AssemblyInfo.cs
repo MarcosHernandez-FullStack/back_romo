8b350621370e528eaeba97843baedb3dd51b340e
MarcosHernandez-FullStack <marcoshernandez171711@gmail.com>
2026-06-04 00:37:51 -0500

    Correccion de script de base de datos

--- BackRomo.Domain/obj/Debug/net10.0/BackRomo.Domain.AssemblyInfo.cs
+++ BackRomo.Domain/obj/Debug/net10.0/BackRomo.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackRomo.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a03d93ceba31d2b86e6008513c8782b071d15ce9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfdc1f0cfa39cb4c6df60168bf0bd7e67d57c5e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackRomo.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackRomo.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
